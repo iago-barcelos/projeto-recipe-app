7e7a5d80a9b139bfb2a86d28bcee1a9b6c99cc52
Iago Barcelos <iago.cmpb@gmail.com>
2023-09-21 18:27:49 -0300

    atualizações nos testes

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import App from '../App';
+import DoneRecipes from '../pages/DoneRecipes';
+import Drinks from '../pages/Drinks';
+import FavoriteRecipes from '../pages/FavoriteRecipes';
+import Header from '../components/Header';
+import HomeMeal from '../pages/HomeMeal';
+import Profile from '../pages/Profile';
 
 const emailTestId = 'email-input';
 const passwordTestId = 'password-input';
 const submitButtonTestId = 'login-submit-btn';
 const validEmail = 'email@valido.com';
+const pageTitle = 'page-title';
+const searchIcon = 'Search Icon';
 
 test('Verifica se existe uma tela de login', () => {
   render(
@@ -117,4 +125,159 @@ test('Navega para homeMeal', async () => {
   const homeMealText = await screen.findByText('Tela de Receitas de Comidas');
 
   expect(await homeMealText).toBeInTheDocument();
+});
+
+describe('Testa componente DoneRecipes', () => {
+  test('Renderiza página com título certo', () => {
+    render(
+      <BrowserRouter>
+        <DoneRecipes />
+      </BrowserRouter>,
+    );
+
+    const pageTitleElement = screen.getByTestId(pageTitle);
+    expect(pageTitleElement).toBeInTheDocument();
+    expect(pageTitleElement.textContent).toBe('Done Recipes');
+  });
+
+  test('Não renderiza ícone de search', () => {
+    render(
+      <BrowserRouter>
+        <DoneRecipes />
+      </BrowserRouter>,
+    );
+
+    const searchIconElement = screen.queryByAltText(searchIcon);
+    expect(searchIconElement).toBeNull();
+  });
+});
+
+describe('Componente Drinks', () => {
+  test('Renderiza a página com o endereço correto', () => {
+    render(
+      <BrowserRouter>
+        <Drinks />
+      </BrowserRouter>,
+    );
+
+    const pageTitleElement = screen.getByTestId(pageTitle);
+    expect(pageTitleElement).toBeInTheDocument();
+    expect(pageTitleElement.textContent).toBe('Drinks');
+  });
+
+  test('Renderiza o icone de pesquisa', () => {
+    render(
+      <BrowserRouter>
+        <Drinks />
+      </BrowserRouter>,
+    );
+
+    const searchIconElement = screen.getByAltText(searchIcon);
+    expect(searchIconElement).toBeInTheDocument();
+  });
+});
+
+describe('Testa componente FavoriteRecipes', () => {
+  test('Renderiza página com título devido', () => {
+    render(
+      <BrowserRouter>
+        <FavoriteRecipes />
+      </BrowserRouter>,
+    );
+
+    const pageTitleElement = screen.getByTestId(pageTitle);
+    expect(pageTitleElement).toBeInTheDocument();
+    expect(pageTitleElement.textContent).toBe('Favorite Recipes');
+  });
+
+  test('Não renderiza ícone de pesquisa', () => {
+    render(
+      <BrowserRouter>
+        <FavoriteRecipes />
+      </BrowserRouter>,
+    );
+
+    const searchIconElement = screen.queryByAltText(searchIcon);
+    expect(searchIconElement).toBeNull();
+  });
+});
+
+describe('Testando o component Header', () => {
+  test('Renderiza pageTitle', () => {
+    const { getByText } = render(<Header pageTitle={ pageTitle } />);
+
+    const pageTitleElement = getByText(pageTitle);
+    expect(pageTitleElement).toBeInTheDocument();
+  });
+
+  test('Renderiza icone de profile', () => {
+    const { getByAltText } = render(<Header pageTitle="Title" />);
+
+    const profileIconElement = getByAltText('Profile Icon');
+    expect(profileIconElement).toBeInTheDocument();
+  });
+
+  test('Renderiza icone de pesquisa se showSearchIcon tiver valor true', () => {
+    const { getByAltText } = render(<Header pageTitle="Title" showSearchIcon />);
+
+    const searchIconElement = getByAltText(searchIcon);
+    expect(searchIconElement).toBeInTheDocument();
+  });
+
+  test('Não renderiza icone de pesquisa se showSearchIcon tiver valor false', () => {
+    const { queryByAltText } = render(<Header pageTitle="Title" showSearchIcon={ false } />);
+
+    const searchIconElement = queryByAltText(searchIcon);
+    expect(searchIconElement).toBeNull();
+  });
+});
+
+describe('Componente homeMeal', () => {
+  test('Renderiza página com título correto', () => {
+    render(
+      <BrowserRouter>
+        <HomeMeal />
+      </BrowserRouter>,
+    );
+
+    const pageTitleElement = screen.getByTestId(pageTitle);
+    expect(pageTitleElement).toBeInTheDocument();
+    expect(pageTitleElement.textContent).toBe('Meals');
+  });
+
+  test('Renderiza ícone de pesquisa', () => {
+    render(
+      <BrowserRouter>
+        <HomeMeal />
+      </BrowserRouter>,
+    );
+
+    const searchIconElement = screen.getByAltText(searchIcon);
+    expect(searchIconElement).toBeInTheDocument();
+  });
+});
+
+describe('Componente Profile', () => {
+  test('Renderiza página com título correto', () => {
+    render(
+      <BrowserRouter>
+        <Profile />
+      </BrowserRouter>,
+    );
+
+    const pageTitleElement = screen.getByTestId(pageTitle);
+    expect(pageTitleElement).toBeInTheDocument();
+    expect(pageTitleElement.textContent).toBe('Profile');
+  });
+
+  test('Não renderiza ícone de pesquisa', () => {
+    render(
+      <BrowserRouter>
+        <Profile />
+      </BrowserRouter>,
+    );
+
+    const searchIconElement = screen.queryByAltText(searchIcon);
+    expect(searchIconElement).toBeNull();
+  });
 });
