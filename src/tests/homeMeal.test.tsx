502f48d189ef8f7b80a5681d16a639b37e036ad5
Iago Barcelos <iago.cmpb@gmail.com>
2023-09-21 18:05:46 -0300

    atualizando req7

--- src/tests/homeMeal.test.tsx
+++ src/tests/homeMeal.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import HomeMeal from '../pages/homeMeal';
+import HomeMeal from '../pages/HomeMeal';
 
 describe('Componente homeMeal', () => {
   test('Renderiza página com título correto', () => {
