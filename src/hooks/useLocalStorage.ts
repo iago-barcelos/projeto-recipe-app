7e73704a77adfa76d79317159eeb9379a3905e82
Gabriel Muniz <gabrielmfd@gmail.com>
2023-09-21 13:06:43 -0300

    Feat: cria arquivo types.ts para elaboração de tipos da aplicação

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react';
+import { UserInfoType, CocktailType, MealType } from '../types';
 
 // mudar tipo de initialValue para o tipo de elemento que vai entrar através do envio do form
 
-const useLocalStorage = (key: string, initialValue: string) => {
+const useLocalStorage = (
+  key: string,
+  initialValue: UserInfoType | CocktailType | MealType,
+) => {
   const [localStorageInfo, setLocalStorageInfo] = useState(() => {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : initialValue;
