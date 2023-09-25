f01da379e3dbe3c5fa4b88207fc232fb4700d8fc
Gabriel Muniz <gabrielmfd@gmail.com>
2023-09-25 15:37:45 -0300

    Fix: refatoração componente FilterBar

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -24,34 +24,47 @@ function FilterBar({ page }: FilterBarProps) {
     const endpoint = toggle === 'meals' ? 'themealdb' : 'thecocktaildb';
     const filterResult = await getFetch(`https://www.${endpoint}.com/api/json/v1/1/list.php?c=list`, '');
     const filteredCategories = filterResult[toggle].slice(0, 5);
-    if (toggle === 'drinks') {
-      filteredCategories[3] = { strCategory: 'Other-Unknown' };
-    }
     setCategories(filteredCategories);
   };
 
   useEffect(() => {
     getByCategories(page);
   }, [page]);
+  console.log(categories);
 
   return (
     <>
       <button data-testid="All-category-filter">
-        <img src={ `../src/images/FilterBarIcons/all{${page}}.svg` } alt="" />
+        <img
+          src={ `../src/images/FilterBarIcons/all-${
+            page === 'meals' ? 'meals' : 'drinks'
+          }.svg` }
+          alt=""
+        />
         All
         {' '}
         {page}
       </button>
       {categories.map((category, i) => (
-        <button key={ i } data-testid={ `${category.strCategory}-category-filter` }>
+        <button
+          key={ i }
+          data-testid={ `${
+            category.strCategory.includes('/')
+              ? 'Other/Unknown'
+              : category.strCategory
+          }-category-filter` }
+        >
           <img
-            src={ `../src/images/FilterBarIcons/${category.strCategory}.svg` }
+            src={ `../src/images/FilterBarIcons/${
+              category.strCategory.includes('/')
+                ? 'Other'
+                : category.strCategory
+            }.svg` }
             alt={ category.strCategory }
           />
           {category.strCategory}
         </button>
       ))}
-
     </>
   );
 }
