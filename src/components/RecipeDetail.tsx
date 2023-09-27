22018b3fbd36cc7b24e7c9045908d8325e3ac236
Gabriel Muniz <gabrielmfd@gmail.com>
2023-09-27 17:46:43 -0300

    Ainda trabalhando no componente RecipeDetail

--- src/components/RecipeDetail.tsx
+++ src/components/RecipeDetail.tsx
@@ -8,14 +8,23 @@ function RecipeDetail() {
   const { recipeDetail } = useRecipeDetails(checkID);
   const checkMealRecipe = recipeDetail as MealRecipeDetailsType;
   const checkDrinkRecipe = recipeDetail as DrinksRecipeDetailsType;
+  const drinkIngredientList = checkDrinkRecipe?.drinks
+    .map((recipe) => Object.keys(recipe)
+      .filter((keyName) => keyName.includes('strIngredient')));
+  const drinkMeasureList = checkDrinkRecipe?.drinks
+    .map((recipe) => Object.keys(recipe)
+      .filter((keyName) => keyName.includes('strMeasure')));
   const mapedDrinkRecipe = checkDrinkRecipe?.drinks.map((details) => ({
+    id: details.idDrink,
     name: details.strDrink,
     img: details.strDrinkThumb,
     category: details.strCategory,
     instructions: details.strInstructions,
-    ingredients: '',
+    ingredients: drinkIngredientList,
+    measure: drinkMeasureList,
   }));
 
+  console.log(mapedDrinkRecipe);
   return (
     <>
       <h1>Recipe Detail</h1>
@@ -24,6 +33,14 @@ function RecipeDetail() {
         {' '}
         {id as string}
       </h2>
+      {mapedDrinkRecipe?.map((detail) => (
+        <>
+          <h1>{detail.name}</h1>
+          <h3>{detail.category}</h3>
+          <img key={ detail.id } src={ detail.img } alt="" />
+          <p>{detail.instructions}</p>
+        </>
+      ))}
     </>
   );
 }
