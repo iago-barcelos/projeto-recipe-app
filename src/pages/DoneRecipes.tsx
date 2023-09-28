e6c6496a66dea07da2c36d27f39f20ad4fcb6ee8
Pedro Prudencio <pedroprudencio.pp@gmail.com>
2023-09-28 17:58:11 -0300

    requisito 4

--- src/pages/DoneRecipes.tsx
+++ src/pages/DoneRecipes.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
-import { RecipeType } from '../types'; // Certifique-se de que a tipagem esteja adequada às suas necessidades
+import { RecipeType } from '../types';
 
 function DoneRecipes() {
   const [doneRecipes, setDoneRecipes] = useState<RecipeType[]>([]);
@@ -21,6 +21,7 @@ function DoneRecipes() {
       <button data-testid="filter-by-all-btn">All</button>
       <button data-testid="filter-by-meal-btn">Meals</button>
       <button data-testid="filter-by-drink-btn">Drinks</button>
+
       {doneRecipes.map((recipe, index) => (
         <div key={ index }>
           <img
@@ -28,16 +29,25 @@ function DoneRecipes() {
             alt={ recipe.name }
             data-testid={ `${index}-horizontal-image` }
           />
-          <p data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</p>
           <h2 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h2>
+          <p
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            {`${recipe.nationality} - ${recipe.category}`}
+          </p>
           <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
-          <button data-testid={ `${index}-horizontal-share-btn` }>Compartilhar</button>
-
-          {recipe.tags.map((tag, tagIndex) => (
+          {recipe.tags.slice(0, 2).map((tag, tagIndex) => (
             <span key={ tagIndex } data-testid={ `${index}-${tag}-horizontal-tag` }>
               {tag}
             </span>
           ))}
+          <button>
+            <img
+              data-testid={ `${index}-horizontal-share-btn` }
+              src="src/images/shareIcon.svg"
+              alt="Compartilhar"
+            />
+          </button>
         </div>
       ))}
     </div>
--- src/types.ts
+++ src/types.ts
@@ -99,62 +99,6 @@ export type MealRecipeDetailsType = {
   ]
 };
 
-export type DrinksRecipeDetailsType = {
-  drinks: [
-    {
-      dateModified: string,
-      idDrink: string,
-      strAlcoholic: string,
-      strCategory: string,
-      strCreativeCommonsConfirmed: string,
-      strDrink: string,
-      strDrinkAlternate: null
-      strDrinkThumb: string,
-      strGlass: string,
-      strIBA: null,
-      strImageAttribution: null,
-      strImageSource: null,
-      strIngredient1: string,
-      strIngredient2: string,
-      strIngredient3: string,
-      strIngredient4: null,
-      strIngredient5: null,
-      strIngredient6: null,
-      strIngredient7: null,
-      strIngredient8: null,
-      strIngredient9: null,
-      strIngredient10: null,
-      strIngredient11: null,
-      strIngredient12: null,
-      strIngredient13: null,
-      strIngredient14: null,
-      strIngredient15: null,
-      strInstructions: string,
-      strInstructionsDE:string,
-      strInstructionsES:null,
-      strInstructionsFR:null,
-      strInstructionsIT:string,
-      strMeasure1: string
-      strMeasure2: null
-      strMeasure3: null
-      strMeasure4: null
-      strMeasure5: null
-      strMeasure6: null
-      strMeasure7: null
-      strMeasure8: null
-      strMeasure9: null
-      strMeasure10: null
-      strMeasure11: null
-      strMeasure12: null
-      strMeasure13: null
-      strMeasure14: null
-      strMeasure15: null
-      strTags: null
-      strVideo: null
-    },
-  ]
-};
-
 export type FormatedRecipe = {
   id: string;
   name: string;
@@ -179,66 +123,6 @@ export type RecipeData = {
 
 export type InProgressType = {
   [key: string] : RecipeData;
-};
-
-export type MealRecipeDetailsType = {
-  meals: [
-    {
-      dateModified:null,
-      idMeal:string,
-      strArea: string
-      strCategory:string
-      strCreativeCommonsConfirmed: null
-      strDrinkAlternate: null
-      strImageSource: null
-      strIngredient1: string
-      strIngredient2: string
-      strIngredient3: string
-      strIngredient4: string
-      strIngredient5: string
-      strIngredient6: string
-      strIngredient7: string
-      strIngredient8: string
-      strIngredient9: string
-      strIngredient10: string
-      strIngredient11: string
-      strIngredient12: string
-      strIngredient13: string
-      strIngredient14: string
-      strIngredient15: string
-      strIngredient16: string
-      strIngredient17: string
-      strIngredient18: string
-      strIngredient19: string
-      strIngredient20: string
-      strInstructions: string
-      strMeal: string
-      strMealThumb: string
-      strMeasure1: string
-      strMeasure2: string
-      strMeasure3: string
-      strMeasure4: string
-      strMeasure5: string
-      strMeasure6: string
-      strMeasure7: string
-      strMeasure8: string
-      strMeasure9: string
-      strMeasure10: string
-      strMeasure11: string
-      strMeasure12: string
-      strMeasure13: string
-      strMeasure14: string
-      strMeasure15: string
-      strMeasure16: string
-      strMeasure17: string
-      strMeasure18: string
-      strMeasure19: string
-      strMeasure20: string
-      strSource: string
-      strTags: string
-      strYoutube: string
-    },
-  ]
 };
 
 export type DrinksRecipeDetailsType = {
@@ -298,6 +182,7 @@ export type DrinksRecipeDetailsType = {
 };
 
 export type RecipeType = {
+  nationality: any;
   name: string;
   image: string;
   category: string;
