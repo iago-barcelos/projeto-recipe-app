0c6c47f2aa81c1854cdd482172eb962207ae1200
Gabriel Muniz <gabrielmfd@gmail.com>
2023-09-29 14:01:29 -0300

    Finaliza requisito 27

--- src/components/RecipeDetail.tsx
+++ src/components/RecipeDetail.tsx
@@ -12,11 +12,9 @@ import useFetch from '../hooks/useFetch';
 function RecipeDetail() {
   const { id } = useParams();
   const mealOrDrink = window.location.pathname.includes('meals');
-  const { recommendedDrinks, recommendedMeals } = useFetch('api/json/v1/1/search.php?s=');
-  const drinksCarousel = recommendedDrinks.slice(0, 6);
-  const mealsCarousel = recommendedMeals.slice(0, 6);
 
   // recebe os dados da Api via recipeDetail e depois trata, usando as funções formatMealRecipe e formatDrinkRecipe no useEffect
+
   const { mealRecipeDetail, drinkRecipeDetail } = useRecipeDetails(id as string);
   const [formatedRecipe, setFormatedRecipe] = useState<FormatedRecipe>([]);
 
@@ -30,6 +28,29 @@ function RecipeDetail() {
     }
   }, [mealRecipeDetail, drinkRecipeDetail, mealOrDrink]);
 
+  // faz fetch na API para pegar as bebidas e comidas recomendadas e depois lista as 6 primeiras para o carrossel
+
+  const { recommendedDrinks, recommendedMeals } = useFetch('api/json/v1/1/search.php?s=');
+  const drinksCarousel = recommendedDrinks.slice(0, 6);
+  const mealsCarousel = recommendedMeals.slice(0, 6);
+
+  // cria estado de contagem para o indice do carrossel
+  const [counter, setCounter] = useState(0);
+
+  // const handleNextCount = () => {
+  //   setCounter((count) => count + 2);
+  // };
+
+  // const handlePreviousCount = () => {
+  //   setCounter((count) => (count > 0 ? count - 2 : count));
+  // };
+
+  // useEffect(() => {
+  //   if (counter === 6) {
+  //     setCounter(0);
+  //   }
+  // }, [counter]);
+  // console.log(counter);
   return (
     <>
       <h1>Recipe Detail</h1>
@@ -42,7 +63,9 @@ function RecipeDetail() {
         <div key={ recipe.id }>
           <h1 data-testid="recipe-title">{recipe.name}</h1>
           <h3 data-testid="recipe-category">{recipe.category}</h3>
-          {recipe.alcoholic && <h3 data-testid="recipe-category">{recipe.alcoholic}</h3>}
+          {recipe.alcoholic && (
+            <h3 data-testid="recipe-category">{recipe.alcoholic}</h3>
+          )}
           <img
             data-testid="recipe-photo"
             key={ recipe.id }
@@ -81,6 +104,42 @@ function RecipeDetail() {
           </section>
         </div>
       ))}
+      {mealOrDrink && (
+        <section>
+          {drinksCarousel.map((drink, i) => (
+            <div
+              key={ i }
+              data-testid={ `${i}-recommendation-card` }
+              style={ {
+                display:
+                  i === counter || i === counter + 1 ? 'inline-block' : 'none',
+              } }
+            >
+              <img src={ drink?.strDrinkThumb } alt={ drink?.strDrink } />
+              <h3 data-testid={ `${i}-recommendation-title` }>
+                {drink?.strDrink}
+              </h3>
+            </div>
+          ))}
+        </section>
+      )}
+      {!mealOrDrink && (
+        <section>
+          {mealsCarousel.map((meal, i) => (
+            <div
+              key={ i }
+              data-testid={ `${i}-recommendation-card` }
+              style={ {
+                display:
+                  i === counter || i === counter + 1 ? 'inline-block' : 'none',
+              } }
+            >
+              <img src={ meal?.strMealThumb } alt={ meal?.strMeal } />
+              <h3 data-testid={ `${i}-recommendation-title` }>{meal?.strMeal}</h3>
+            </div>
+          ))}
+        </section>
+      )}
     </>
   );
 }
